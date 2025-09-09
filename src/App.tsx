f39a6967fde74cb8e5d56cce6dd5b5a211ae6495
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2025-09-09 16:04:48 -0300

    feat: add registro details route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Registros from "./pages/Registros";
+import RegistroDetails from "./pages/RegistroDetails";
 import { Toaster } from "@/components/ui/sonner";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/registros" element={<Registros />} />
+				<Route path="/registros/:id" element={<RegistroDetails />} />
 			</Routes>
 			<Toaster />
 		</>
