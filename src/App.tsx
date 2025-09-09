90567127e07656666691b91e73cf565e79be36bf
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2025-09-09 16:01:37 -0300

    feat: add toaster config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Registros from "./pages/Registros";
+import { Toaster } from "@/components/ui/sonner";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/registros" element={<Registros />} />
-		</Routes>
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/registros" element={<Registros />} />
+			</Routes>
+			<Toaster />
+		</>
 	);
 }
 
